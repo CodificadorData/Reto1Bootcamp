4ca328c54b9cf7bde8f08e91274f6357350447c9
Christian Alexander Morante Santander <cmorante@LIM-FVFHL3GXQ05N.local>
2022-06-20 15:56:03 -0500

    Mi primer avance

---
+++ ga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ga
+//
+//  Created by Christian Alexander Morante Santander on 19/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
